3ceb3e1059609ecb95698300b874b5ddd9672a99
Jhonny Baptistiolli <baptistiolli@gmail.com>
2018-07-23 20:04:48 -0300

    add routes

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,39 @@
 
 Route::get('/', 'PostsController@index');
 
-Route::get('admin', 'PostsAdminController@index');
-Route::get('admin/create', 'PostsAdminController@create');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+	Route::group(['prefix' => 'posts'], function() {
+		Route::get('', 
+			['as' => 'admin.posts.index', 'uses' => 'PostsAdminController@index']
+		);
+
+		Route::get('create', 
+			['as' => 'admin.posts.create', 'uses' => 'PostsAdminController@create']
+		);
+
+		Route::post('store',
+			['as' => 'admin.posts.store', 'uses' => 'PostsAdminController@store']
+		);
+
+		Route::get('edit/{id}',
+			['as' => 'admin.posts.edit', 'uses' => 'PostsAdminController@edit']
+		);
+
+		Route::put('update/{id}',
+			['as' => 'admin.posts.update', 'uses' => 'PostsAdminController@update']
+		);
+
+		Route::get('destroy/{id}', 
+			['as' => 'admin.posts.destroy', 'uses' => 'PostsAdminController@destroy']
+		);
+	});
+	
+});
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/verify-user/{code}',
+	'Auth\RegisterController@activateUser')->name('activate.user');
