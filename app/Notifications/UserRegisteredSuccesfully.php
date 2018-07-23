d463392d620d8134f3d07da3408db6206f07a9e6
Jhonny Baptistiolli <baptistiolli@gmail.com>
2018-07-23 20:09:08 -0300

    notifications

---
+++ app/Notifications/UserRegisteredSuccesfully.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Blog\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class UserRegisteredSuccesfully extends Notification
+{
+    use Queueable;
+
+    /*
+     * @var User
+     */
+    protected $user;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $user = $this->user;
+        return (new MailMessage)
+                    ->from(env('ADMIN_MAIL'))
+                    ->subject('Nova conta criada com sucesso')
+                    ->greeting(sprintf('Hello %s', $user->name))
+                    ->line('Você se registrou com sucesso. Por favor, ative a sua conta.')
+                    ->action('Clique aqui', route('activate.user', $user->actionva_code))
+                    ->line('Obrigador por usar nosso APP!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
